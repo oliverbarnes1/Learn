de49fb9d84b5be44d2fd200901a27acb5a979686
Oliver barnes <oliverbarnes124@gmail.com>
2022-02-06 15:33:16 +0100

    pages done and set up perfect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'services', to: 'pages#services'
+  get 'news', to: 'pages#news'
+  get 'contact', to: 'pages#contact'
 end
 
-ahhhhhhhhhhhhhhhhh
+
 
 
