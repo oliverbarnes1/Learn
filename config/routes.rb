48f46b2bc89ae3a78797f1c4d6c61d3bea2b6fcb
Oliver barnes <oliverbarnes124@gmail.com>
2022-02-06 13:15:02 +0100

    ggrsj

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-hi bud
+hi bud we have to keep tryin now its time for a branch
